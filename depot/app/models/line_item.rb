073d5198bbef851d064df204e3cf6f268642c045
Shaun <seyyes2000@hotmail.com>
2017-07-23 16:42:19 -0400

    work on orders, but cart and order has disappeared

--- depot/app/models/line_item.rb
+++ depot/app/models/line_item.rb
@@ -1,6 +1,10 @@
 class LineItem < ActiveRecord::Base
+  belongs_to :order
   belongs_to :product
   belongs_to :cart
+
+  # validates :product_id, presence: true
+  # validates :cart_id, presence: true, on: :create
 
   def total_price
     product.price * quantity
--- depot/test/controllers/orders_controller_test.rb
+++ depot/test/controllers/orders_controller_test.rb
@@ -28,14 +28,13 @@ class OrdersControllerTest < ActionDispatch::IntegrationTest
 
   test "should create order" do
     assert_difference('Order.count') do
-      post orders_url, params: { order: { address: @order.address, 
-                                          email: @order.email, 
-                                          name: @order.name, 
-                                          pay_type: @order.pay_type }
-      }
+      post :create, order: { address: @order.address, 
+                              email: @order.email, 
+                              name: @order.name, 
+                              pay_type: @order.pay_type }
     end
 
-    assert_redirected_to order_url(Order.last)
+    assert_redirected_to store_path
   end
 
   test "should show order" do
