b5aae2b93f4020bd42e2a459338d582d988545d7
Shaun <seyyes2000@hotmail.com>
2017-07-23 07:03:03 -0400

    add checkout forms

--- depot/app/models/order.rb
+++ depot/app/models/order.rb
@@ -1,2 +1,7 @@
-class Order < ApplicationRecord
+class Order < ActiveRecord::Base
+  PAYMENT_TYPES = [ "Check", "Credit Card", "Purchase Order" ]
+  has_many :line_items, dependent: :destroy
+
+  validates :name, :address, :email, presence: true
+  validates :pay_type, inclusion: PAYMENT_TYPES
 end
