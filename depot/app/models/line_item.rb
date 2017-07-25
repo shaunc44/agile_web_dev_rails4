2013b3962128fd6b340df7b4726db3a446835291
Shaun <seyyes2000@hotmail.com>
2017-07-25 17:38:32 -0400

    finish creating order functionality

--- depot/app/models/line_item.rb
+++ depot/app/models/line_item.rb
@@ -1,11 +1,11 @@
 class LineItem < ActiveRecord::Base
-  # why is the line of code below erasing the cart?????????????????
-  # belongs_to :order
+  # why is 'belongs_to :order' erasing the cart?????????????????
+  belongs_to :order, optional: true
   belongs_to :product
   belongs_to :cart
 
-  # validates :product_id, presence: true
-  # validates :cart_id, presence: true, on: :create
+  validates :product_id, presence: true
+  validates :cart_id, presence: true, on: :create
 
   def total_price
     product.price * quantity
--- depot/config/routes.rb
+++ depot/config/routes.rb
@@ -2,11 +2,12 @@ Depot::Application.routes.draw do
   resources :orders
   resources :line_items
   resources :carts
+
   get "store/index"
-  resources :products
+  resources :products do
+    get :who_bought, on: :member
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'store#index', as: 'store'
-
-
 end
