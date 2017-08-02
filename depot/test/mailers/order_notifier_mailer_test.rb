941756e50016c577bf8fa25995c9f0363d1e64dd
Shaun <seyyes2000@hotmail.com>
2017-08-02 12:08:02 -0400

    fix failures related to email

--- depot/test/mailers/order_notifier_mailer_test.rb
+++ depot/test/mailers/order_notifier_mailer_test.rb
@@ -5,7 +5,7 @@ class OrderNotifierMailerTest < ActionMailer::TestCase
     mail = OrderNotifierMailer.received(orders(:one))
     assert_equal "Pragmatic Store Order Confirmation", mail.subject
     assert_equal ["to@example.org"], mail.to
-    assert_equal ["from@example.com"], mail.from
+    assert_equal ["shauncox44@gmail.com"], mail.from
     assert_match /1 x Programming Ruby 1.9/, mail.body.encoded
   end
 
@@ -13,7 +13,7 @@ class OrderNotifierMailerTest < ActionMailer::TestCase
     mail = OrderNotifierMailer.shipped(orders(:one))
     assert_equal "Pragmatic Store Order Shipped", mail.subject
     assert_equal ["to@example.org"], mail.to
-    assert_equal ["from@example.com"], mail.from
+    assert_equal ["shauncox44@gmail.com"], mail.from
     assert_match /<td>1&times;<\/td>\s*<td>Programming Ruby 1.9<\/td>/, mail.body.encoded
   end
 
